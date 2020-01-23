5deb8c0a72eb545822fed2467cc4a0fea6c5e997
unknown <1165240284@qq.com>
2020-01-23 23:32:40 +0800

    打印系统状态路由

--- FlaskShowRedis.py
+++ FlaskShowRedis.py
@@ -1,9 +1,19 @@
 from flask import Flask
+import sys
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Test'
+
+@app.route('/syspara')
+def getSysPara():
+    '''
+    显示系统参数
+    :return:
+    '''
+    pythonVersion=sys.version
+    return pythonVersion
 
 if __name__ == '__main__':
     app.run()#执行flask的运行
