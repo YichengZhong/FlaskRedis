be7f7e129f9f7c2ba0da33d06e8ac73c3fc7fca8
YichengZhong <1165240284@qq.com>
2020-02-08 16:20:56 +0800

    根据不同命令进行匹配

--- hashCommand.py
+++ hashCommand.py
@@ -1,3 +1,24 @@
+def hash_get_func(command):
+    pass
+
+def hash_set_func(command):
+    pass
+
+def hash_mget_func(command):
+    pass
+
+def hash_mset_func(command):
+    pass
+
+def hash_del_func(command):
+    pass
+
+dic_string_commandswitch = {"hash_getcommand":hash_get_func,
+                            "hash_setcommand":hash_set_func,
+                            "hash_mgetscommand":hash_mget_func,
+                            "hash_msetcommand": hash_mset_func,
+                            "hash_delcommand":hash_del_func}
+
 def hash_func(command):
     '''
     运行hash命令
