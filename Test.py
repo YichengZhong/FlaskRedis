c21c56272a548db4a16fe351639d86b977455266
YichengZhong <1165240284@qq.com>
2020-02-08 11:55:54 +0800

    根据不同命令进行匹配

--- Test.py
+++ Test.py
@@ -4,7 +4,9 @@ from stringCommand import string_func
 from hashCommand import hash_func
 r=redis.StrictRedis(host="192.168.160.140",port=6379,db=0)
 
-dic_commandswitch = {"key_command":key_func,"string_command":string_func,"hash_command":hash_func}
+dic_commandswitch = {"key_command":key_func,
+                     "string_command":string_func,
+                     "hash_command":hash_func}
 
 def commandTran(command):
     '''
--- keyCommand.py
+++ keyCommand.py
@@ -1,3 +1,16 @@
+def key_del_func(command):
+    pass
+
+def key_scan_func(command):
+    pass
+
+def key_exists_func(command):
+    pass
+
+dic_key_commandswitch = {"key_delcommand":key_del_func,
+                         "key_scancommand":key_scan_func,
+                         "key_existscommand":key_exists_func}
+
 def key_func(command):
     '''
     运行key命令
