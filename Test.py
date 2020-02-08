8c977f4749665595270770a7cfb5fadaac2f7050
YichengZhong <1165240284@qq.com>
2020-02-08 10:37:32 +0800

    根据不同命令进行匹配

--- Test.py
+++ Test.py
@@ -1,17 +1,39 @@
 import redis
-r=redis.StrictRedis(host="192.168.160.137",port=6379,db=0)
+r=redis.StrictRedis(host="192.168.160.140",port=6379,db=0)
 
-r.set('foo','1')
-a=r.get('foo')
-print(a)
-r.incr('foo','1')
-a=r.get('foo')
-print(a)
+def key_func(command):
+    '''
+    运行key命令
+    :return:
+    '''
+    print('key_func')
 
-dic={"IPINFO":"10.43.83.106","IPTYPE":"IPV4","UPF":"UPF1"}
-r.hmset("1000",dic)
+def string_func(command):
+    '''
+    运行string命令
+    :return:
+    '''
+    print('string_func')
 
-li=["IPINFO","IPTYPE"]
-print(r.hmget("1000",li))
-print(r.hgetall("1000"))
-# print("测试代码\n")
+def hash_func(command):
+    '''
+    运行hash命令
+    :return:
+    '''
+    print('third_func')
+
+dic_commandswitch = {"key_command":key_func,"string_command":string_func,"hash_command":hash_func}
+
+def commandTran(command):
+    '''
+    根据命令选择指令
+    :param command:
+    :return:
+    '''
+    try:
+        dic_commandswitch["key_command"](command) #执行相应的方法
+    except KeyError as e:
+        print("command is error")
+
+if __name__ == '__main__':
+    commandTran("12")
