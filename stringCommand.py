53d523f49262b47f280f94682408bd3afe177bb8
YichengZhong <1165240284@qq.com>
2020-02-08 16:04:44 +0800

    根据不同命令进行匹配

--- stringCommand.py
+++ stringCommand.py
@@ -1,3 +1,20 @@
+def string_get_func(command):
+    pass
+
+def string_set_func(command):
+    pass
+
+def string_mget_func(command):
+    pass
+
+def string_mset_func(command):
+    pass
+
+dic_string_commandswitch = {"string_getcommand":string_get_func,
+                            "string_setcommand":string_set_func,
+                            "key_mgetscommand":string_mget_func,
+                            "string_msetcommand": string_mset_func}
+
 def string_func(command):
     '''
     运行string命令
