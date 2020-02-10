cc2ffb62ad8c3d22796ba5cfb957d3f7f9a316f0
YichengZhong <1165240284@qq.com>
2020-02-10 19:32:07 +0800

    根据不同命令进行匹配

--- Test.py
+++ Test.py
@@ -2,10 +2,12 @@ from __init_redis__ import redisConnect
 from keyCommand import key_func
 from stringCommand import string_func
 from hashCommand import hash_func
+from wrongCommand import wrong_func
 
 dic_commandswitch = {"key_command":key_func,
                      "string_command":string_func,
-                     "hash_command":hash_func}
+                     "hash_command":hash_func,
+                     "wrong_command":wrong_func}
 
 L_key_command_keywords=["del","scan","exists"]
 L_string_command_keywords=["set","get","mset","mget"]
@@ -17,10 +19,26 @@ def commandTran(command,redisConnect):
     :param command:
     :return:
     '''
+    L_command_words = command.split(' ')
+    if(len(L_command_words)<2):
+        print("command len is error\n")
+        return
+
+    command_type="wrong_command"
+
+    if(L_command_words[0] in L_key_command_keywords):
+        command_type="key_command"
+    elif (L_command_words[0] in L_string_command_keywords):
+        command_type= "string_command"
+    elif (L_command_words[0] in L_hash_command_keywords):
+        command_type="hash_command"
+    else:
+        command_type = "wrong_command"
+
     try:
-        dic_commandswitch["key_command"](command) #执行相应的方法
+        dic_commandswitch[command_type](command,redisConnect) #执行相应的方法
     except KeyError as e:
         print("command is error")
 
 if __name__ == '__main__':
-    commandTran("12")
+    commandTran("set runoobkey redis",redisConnect)
---
+++ wrongCommand.py
@@ -0,0 +1,6 @@
+def wrong_func(command,redisConnect):
+    '''
+    执行命令不在范围之内
+    :return:
+    '''
+    print('wrong_func')
